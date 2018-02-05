65ba57b7418418804f9f00aa9b39613aaac0fb69
Witold Markowski <witold.a.markowski@gmail.com>
2018-02-05 22:58:54 +0000

    Format with NASA formatter.

--- src/CRDSParser.h
+++ src/CRDSParser.h
@@ -18,54 +18,55 @@
 /// * 27.03.2015 Reset RDS data by sending a 0 in blockA in the case the frequency changes.
 /// 
 
-
 #ifndef __CRDSPARSER_H__
 #define __CRDSPARSER_H__
 
 #include <Arduino.h>
 
 /// callback function for passing a ServicenName 
-extern "C" {
-  typedef void(*receiveServicenNameFunction)(char *name);
-  typedef void(*receiveTextFunction)(char *name);
-  typedef void(*receiveTimeFunction)(uint8_t hour, uint8_t minute);
+extern "C"
+{
+   typedef void (*receiveServicenNameFunction)(char *name);
+   typedef void (*receiveTextFunction)(char *name);
+   typedef void (*receiveTimeFunction)(uint8_t hour, uint8_t minute);
 }
-
 
 /// Library for parsing RDS data values and extracting information.
 class CRDSParser
 {
 public:
-  CRDSParser(); ///< create a new object from this class.
+   CRDSParser(); ///< create a new object from this class.
 
-  /// Initialize internal variables before starting or after a change to another channel.
-  void init();
+   /// Initialize internal variables before starting or after a change to another channel.
+   void init();
 
-  /// Pass all available RDS data through this function.
-  void processData(uint16_t block1, uint16_t block2, uint16_t block3, uint16_t block4);
+   /// Pass all available RDS data through this function.
+   void processData(uint16_t block1, uint16_t block2, uint16_t block3,
+         uint16_t block4);
 
-  void attachServicenNameCallback(receiveServicenNameFunction newFunction); ///< Register function for displaying a new Service Name.
-  void attachTextCallback(receiveTextFunction newFunction); ///< Register the function for displaying a rds text.
-  void attachTimeCallback(receiveTimeFunction newFunction); ///< Register function for displaying a new time
+   void attachServicenNameCallback(receiveServicenNameFunction newFunction); ///< Register function for displaying a new Service Name.
+   void attachTextCallback(receiveTextFunction newFunction); ///< Register the function for displaying a rds text.
+   void attachTimeCallback(receiveTimeFunction newFunction); ///< Register function for displaying a new time
 
 private:
-  // ----- actual RDS values
-  uint8_t rdsGroupType, rdsTP, rdsPTY;
-  uint8_t _textAB, _last_textAB, _lastTextIDX;
+   // ----- actual RDS values
+   uint8_t rdsGroupType, rdsTP, rdsPTY;
+   uint8_t _textAB, _last_textAB, _lastTextIDX;
 
-  // Program Service Name
-  char _PSName1[10]; // including trailing '\00' character.
-  char _PSName2[10]; // including trailing '\00' character.
-  char programServiceName[10];    // found station name or empty. Is max. 8 character long.
+   // Program Service Name
+   char _PSName1[10]; // including trailing '\00' character.
+   char _PSName2[10]; // including trailing '\00' character.
+   char programServiceName[10]; // found station name or empty. Is max. 8 character long.
 
-  receiveServicenNameFunction _sendServiceName; ///< Registered ServiceName function.
-  receiveTimeFunction _sendTime; ///< Registered Time function.
-  receiveTextFunction _sendText;
+   receiveServicenNameFunction _sendServiceName; ///< Registered ServiceName function.
+   receiveTimeFunction _sendTime; ///< Registered Time function.
+   receiveTextFunction _sendText;
 
-  uint16_t _lastRDSMinutes; ///< last RDS time send to callback.
+   uint16_t _lastRDSMinutes; ///< last RDS time send to callback.
 
-  char _RDSText[64 + 2];
+   char _RDSText[64 + 2];
 
-}; //CRDSParser
+};
+//CRDSParser
 
 #endif //__CRDSPARSER_H__
